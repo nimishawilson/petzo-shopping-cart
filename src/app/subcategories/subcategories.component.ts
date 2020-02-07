eda4fd698f87257163995c4d402063fb2e1d673f
Nimisha Wilson <nimishawilson123@gmail.com>
2020-02-07 15:05:18 +0530

    feat: pass parameter through activated routing to subcategories and filtered and show corresponding subcategories

--- src/app/subcategories/subcategories.component.ts
+++ src/app/subcategories/subcategories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-subcategories',
@@ -6,20 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subcategories.component.scss']
 })
 export class SubcategoriesComponent implements OnInit {
-   subCategories = [ 
-    { catName: 'cat', subCat: 'cat food'} ,
-    { catName: 'cat', subCat: 'adoption'},
-    { catName: 'cat', subCat: 'toys'},
-    { catName: 'dog', subCat: 'dog food'},
-    { catName: 'dog', subCat: 'adoption'},
-    { catName: 'dog', subCat: 'toys'}
-  ];
-  
+ 
 
-  constructor() { }
+  public subCategoriesfiltered = [];
+  
+ 
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+    let categoryNameParam = this.route.snapshot.params['catname'];
+
+    var categorySubcategory = [ 
+    { catName: 'cat', subCat: 'cat food', imgPath: "../../assets/images/cat.png"} ,
+    { catName: 'cat', subCat: 'adoption', imgPath: "../../assets/images/cat.png"},
+    { catName: 'cat', subCat: 'toys', imgPath: "../../assets/images/cat.png"},
+    { catName: 'dog', subCat: 'dog food', imgPath: "../../assets/images/dog.png"},
+    { catName: 'dog', subCat: 'adoption', imgPath: "../../assets/images/dog.png"},
+    { catName: 'dog', subCat: 'toys', imgPath: "../../assets/images/dog.png"}
+  ];
+    // refer filtering array objects  https://alligator.io/js/filter-array-method/
+
+  this.subCategoriesfiltered = categorySubcategory.filter(function(sub) {
+    return sub.catName == categoryNameParam;
+  });
+
   
+
   }
+
+
+  
 
 }
