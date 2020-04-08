bb8034e91fd842ef3e8a1f4358f561e52d174f58
Nimisha Wilson <nimishawilson123@gmail.com>
2020-04-08 15:11:57 +0530

    clean code

--- src/app/subcategories/subcategories.component.ts
+++ src/app/subcategories/subcategories.component.ts
@@ -7,12 +7,8 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./subcategories.component.scss']
 })
 export class SubcategoriesComponent implements OnInit {
- 
-
   public subCategoriesfiltered = [];
   public categoryName;
-  
- 
 
   constructor(private route: ActivatedRoute) { }
 
@@ -20,25 +16,19 @@ export class SubcategoriesComponent implements OnInit {
 
     let categoryNameParam = this.route.snapshot.params['catname'];
 
-    var categorySubcategory = [ 
-    { catName: 'cat', subCat: 'cat food', subCatcode: 1, imgPath: "../../assets/images/cat.png"} ,
-    { catName: 'cat', subCat: 'adoption', subCatcode: 2, imgPath: "../../assets/images/cat.png"},
-    { catName: 'cat', subCat: 'toys', subCatcode: 3, imgPath: "../../assets/images/cat.png"},
-    { catName: 'dog', subCat: 'dog food', subCatcode: 4, imgPath: "../../assets/images/dog.png"},
-    { catName: 'dog', subCat: 'adoption', subCatcode: 5, imgPath: "../../assets/images/dog.png"},
-    { catName: 'dog', subCat: 'toys', subCatcode: 6, imgPath: "../../assets/images/dog.png"}
-  ];
+    var categorySubcategory = [
+      { catName: 'cat', subCat: 'cat food', subCatcode: 1, imgPath: "../../assets/images/cat.png" },
+      { catName: 'cat', subCat: 'adoption', subCatcode: 2, imgPath: "../../assets/images/cat.png" },
+      { catName: 'cat', subCat: 'toys', subCatcode: 3, imgPath: "../../assets/images/cat.png" },
+      { catName: 'dog', subCat: 'dog food', subCatcode: 4, imgPath: "../../assets/images/dog.png" },
+      { catName: 'dog', subCat: 'adoption', subCatcode: 5, imgPath: "../../assets/images/dog.png" },
+      { catName: 'dog', subCat: 'toys', subCatcode: 6, imgPath: "../../assets/images/dog.png" }
+    ];
     // refer filtering array objects  https://alligator.io/js/filter-array-method/
-
-  this.subCategoriesfiltered = categorySubcategory.filter(function(sub) {
-    return sub.catName == categoryNameParam;
-  });
-
-  this.categoryName = categoryNameParam;
-
+    this.subCategoriesfiltered = categorySubcategory.filter(function (sub) {
+      return sub.catName == categoryNameParam;
+    });
+    this.categoryName = categoryNameParam;
   }
-
-
-  
 
 }
